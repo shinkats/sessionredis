133d1836f31409c392ccdf7e4b3f4642835848e9
shinkats <qwerty.mnbvcx2009@gmail.com>
2021-12-19 00:32:34 +0900

    add Serializable interface to LoginUser to store it in SessionRedis

--- src/main/kotlin/com/example/sessionredis/security/LoginUser.kt
+++ src/main/kotlin/com/example/sessionredis/security/LoginUser.kt
@@ -1,7 +1,8 @@
 package com.example.sessionredis.security
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority
+import java.io.Serializable
 
-class LoginUser(val id: Int, roles: List<String>) {
+class LoginUser(val id: Int, roles: List<String>) : Serializable {
     val authorities = roles.map { SimpleGrantedAuthority(it) }
 }
